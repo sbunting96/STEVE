ad1ed82f0c5deb70198f8378a3f9067d92231cb0
David Gordon <David Gordon>
2016-10-13 16:59:14 -0400

    initial build

---
+++ Assets/base_scene/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public GameObject mainCam;
+
+	public float moveSpeed;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetButton ("Fire1")) {
+			Vector3 forward = new Vector3 (mainCam.transform.forward.x, 0,
+				mainCam.transform.forward.z);
+			transform.position = transform.position + (forward * moveSpeed * Time.deltaTime);
+		}
+	}
+}
+
