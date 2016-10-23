7b18ba52c1207bf269d4a30e44886c5bba863d06
David Gordon <David Gordon>
2016-10-23 14:30:10 -0400

    updated readme

--- Assets/base_scene/Scripts/PlayerController.cs
+++ Assets/base_scene/Scripts/PlayerController.cs
@@ -8,21 +8,36 @@ public class PlayerController : MonoBehaviour
 
 	public float moveSpeed;
 
-	private Rigidbody rb;
+	private float multiplier;
+	private Vector3 curDir, prevDir;
 
 	// Use this for initialization
 	void Awake ()
 	{
-		rb = GetComponent<Rigidbody> ();
+		UpdateDirs();
+		multiplier = 1.0f;
 	}
-	
+
+	void UpdateDirs()
+	{
+		prevDir = curDir;
+		curDir = Vector3.Scale(mainCam.transform.forward, new Vector3(1f, 0, 1f));
+		if (Vector3.Angle (curDir, prevDir) >= 3f)
+			multiplier = 1.0f;
+		else
+			multiplier += 0.1f;
+	}
+
 	// Update is called once per frame
 	void Update ()
 	{
 		if (Input.GetButton ("Fire1")) {
-			Vector3 forward = new Vector3 (mainCam.transform.forward.x, 0,
-				mainCam.transform.forward.z);
+			UpdateDirs ();
+			Vector3 forward = curDir * multiplier;
 			transform.position = transform.position + (forward * moveSpeed * Time.deltaTime);
+		}
+		if (Input.GetKeyDown (KeyCode.M) ){
+			Application.CaptureScreenshot("bouncy_screen.png");
 		}
 	}
 }
